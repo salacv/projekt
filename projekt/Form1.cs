1e508d6b88c0255015823a18dcad2b5291cd4a60
salacv <83805704+salacv@users.noreply.github.com>
2021-05-07 08:50:11 +0200

    Update Form1.cs

--- projekt/Form1.cs
+++ projekt/Form1.cs
@@ -19,7 +19,7 @@ namespace projekt
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Ahoj");
+            MessageBox.Show("AAhoj");
         }
     }
 }
