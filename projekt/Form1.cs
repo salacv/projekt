5753dd47f4330fd620fafba9a8d590e64b38de58
salacv.02 <salacv.02@B121-14>
2021-05-07 09:21:06 +0200

    Pridano tlacitko

--- projekt/Form1.cs
+++ projekt/Form1.cs
@@ -21,5 +21,10 @@ namespace projekt
         {
             MessageBox.Show("Jedeme bomby");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Na pána");
+        }
     }
 }
